31a6423999f7f20b30b4f8e959578e3c457c6f60
Erik van Zijst <erik.van.zijst@gmail.com>
2009-03-20 12:38:34 +1100

    Creating an applet for the anagram solver.

---
+++ src/main/java/cx/prutser/sudoku/capture/AnagramApplet.java
@@ -0,0 +1,14 @@
+package cx.prutser.sudoku.capture;
+
+import javax.swing.*;
+
+/**
+ * @author  Erik van Zijst
+ */
+public class AnagramApplet extends JApplet {
+
+    @Override
+    public void init() {
+        throw new RuntimeException("Not implemented");
+    }
+}
